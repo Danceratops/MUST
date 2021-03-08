fbe59db4bb4323f33e821961138921c3134fac2f
Danielle Reed <dan.reed.993@gmail.com>
2021-03-08 00:42:14 -0500

    Create Home Screen

---
+++ components/Logo.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+
+type LogoProps = {
+  logoType: string;
+};
+
+export default function Logo({ logoType }: LogoProps) {
+  if (logoType === "fullColor") {
+    return (
+      <View>
+        <Image source={require("../assets/FullColorLogo.png")} />
+      </View>
+    );
+  } else if (logoType === "whiteColor") {
+    return (
+      <View>
+        <Image source={require("../assets/WhiteLogo.png")} />
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Image source={require("../assets/FullColorLogo.png")} />
+      </View>
+    );
+  }
+}
