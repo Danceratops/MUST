5ed01a1f3a49282a07d8c26eb90325c8c8602f4d
unknown <agu3@zips.uakron.edu>
2021-04-21 18:21:53 -0400

    Added apiController to components directory

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import APIController from "./components/apiController";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 
